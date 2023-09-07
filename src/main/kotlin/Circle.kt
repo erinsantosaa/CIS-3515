92ba23e25d64f2f93911468ceba9d6391e6025e6
Erin Santosa <tun35949@temple.edu>
2023-09-07 11:58:28 -0400

    added & tested circle class & functions

--- src/main/kotlin/Circle.kt
+++ src/main/kotlin/Circle.kt
@@ -1,2 +1,19 @@
-class Circle {
+import java.util.*
+
+class Circle (_name: String) : Shape(_name), Dimensionable {
+    private var radius = 0.0
+
+    fun setDimensions() {
+        val reader = Scanner(System.`in`)
+        print("Enter a radius: ")
+        radius = reader.nextDouble()
+    }
+
+    override fun getArea(): Double {
+        return 3.14 * radius * radius
+    }
+
+    override fun printDimensions() {
+        println("Radius: $radius")
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,9 @@
 fun main(args: Array<String>) {
+    val circle = Circle("Circle")
+
+    println("Shape: ${circle.name}")
+    circle.setDimensions()
+    circle.printDimensions()
+    println("Area: ${circle.getArea()}\n")
 
 }
